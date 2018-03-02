4137c717c4788d5af460923199a54012200bd42e
Homa Wong <homawong@gmail.com>
2018-03-01 19:15:54 -0800

    fix: prepend logic (#8)
    * fix: prepend logic
    
    Always run tests... you never know...
    
    * test: improve test

--- src/errors.MismatchFile.spec.ts
+++ src/errors.MismatchFile.spec.ts
@@ -11,3 +11,20 @@ test('multiline file will diff by lines', () => {
   const actual = (new MismatchFile('a', 'beep\nboop', 'b', 'beep\nboob blah')).tersify()
   assert(actual.indexOf('+') > 0)
 })
+
+test('multilines are aligned', () => {
+  const a = `
+a
+  b
+    c
+  d
+`
+  const b = `
+a
+  b
+    d
+  e
+`
+  const actual = (new MismatchFile('a', a, 'b', b)).tersify()
+  actual.split('\n').filter(a => a).map(a => a.slice(0,2)).every(a => a === '  ' || a === 'u001b[31m- ' || a === '\u001b[31m\u001b[39m\u001b[32m+ ')
+})
