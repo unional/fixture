db9e42cd131716ef39fd88a1a09476a7f3462b68
Homa Wong <homawong@gmail.com>
2018-03-01 16:52:11 -0800

    feat: improve diff for mismatch file. (#4)

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,5 @@
+import chalk from 'chalk'
+import * as jsdiff from 'diff'
 import { Tersify } from 'tersify'
 
 export class NoCaseFound extends Error {
@@ -9,9 +11,36 @@ export class NoCaseFound extends Error {
 }
 
 export class MismatchFile {
-  constructor(public actualPath, public actual, public expectedPath, public expected) { }
+  constructor(public actualPath: string, public actual: string, public expectedPath: string, public expected: string) { }
   tersify() {
-    return `File '${this.actualPath}' does not match with '${this.expectedPath}'.\n'${this.actual}' != '${this.expected}'`
+    const formattedDiff = this.formatDiff()
+    return `File '${this.actualPath}' does not match with '${this.expectedPath}'.\n\n${formattedDiff}`
+  }
+  private formatDiff() {
+    return this.actual.indexOf('\n') === -1 && this.expected.indexOf('\n') === -1 ? this.formatWordsDiff() : this.formatLinesDiff()
+  }
+  private formatWordsDiff() {
+    return jsdiff.diffWords(this.actual, this.expected)
+      .map(function (part) {
+        if (part.added)
+          return chalk.green(part.value)
+        else if (part.removed)
+          return chalk.red(part.value)
+        else
+          return part.value
+      }).join('')
+  }
+  private formatLinesDiff() {
+    return jsdiff.diffLines(this.actual, this.expected)
+      .map(function (part) {
+        const value = part.value.replace(/(\r\n|\n|\r)/gm, '')
+        if (part.added)
+          return chalk.green(`+ ${value}`)
+        else if (part.removed)
+          return chalk.red(`- ${value}`)
+        else
+          return value
+      }).join('\n')
   }
 }
 
