df6711f66401fb0ca330bb2431e0da5596f5e177
Homa Wong <homawong@gmail.com>
2021-03-28 01:13:15 -0700

    test: add more message to debug ci

--- src/errors.MismatchFile.spec.ts
+++ src/errors.MismatchFile.spec.ts
@@ -3,7 +3,7 @@ import path from 'path'
 
 import { MismatchFile, baseline } from '.'
 
-const cases = {
+const cases: Record<string, string> = {
   'single-line': 'single line file will diff by words',
   'multi-lines': 'multiline file will diff by lines',
   'multi-lines-add': 'multilines (added) are aligned',
@@ -15,12 +15,16 @@ const cases = {
 baseline({
   basePath: 'fixtures/mismatch-file'
 }, c => {
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  test((cases as any)[c.caseName] || c.caseName, () => {
+  test(cases[c.caseName] || c.caseName, () => {
     const source = fs.readFileSync(path.join(c.caseFolder, 'source.yaml'), 'utf-8')
     const target = fs.readFileSync(path.join(c.caseFolder, 'target.yaml'), 'utf-8')
 
-    const mismatch = new MismatchFile('target.yaml', target, 'source.yaml', source, { largeFileThreshold: 5, largeFileAmbientLines: 2 })
+    const mismatch = new MismatchFile(
+      'target.yaml',
+      target,
+      'source.yaml',
+      source,
+      { largeFileThreshold: 5, largeFileAmbientLines: 2 })
 
     fs.writeFileSync(path.join(c.resultFolder, 'result.yaml'), mismatch.formattedDiff)
     return c.match('result.yaml')
