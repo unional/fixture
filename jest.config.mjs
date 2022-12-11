c1c93953c7b0138799f7a9b23a8ea11a4d9a0a92
unional <homawong@gmail.com>
2022-12-11 00:12:17 -0800

    chore: update deps and use repobuddy

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,34 +1,4 @@
+/** @type {import('jest').Config} */
 export default {
-  preset: 'ts-jest/presets/default-esm',
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-      useESM: true,
-    },
-  },
-  collectCoverageFrom: [
-    '<rootDir>/ts/**/*.[jt]s'
-  ],
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-    '#(.*)': '<rootDir>/node_modules/$1'
-  },
-  roots: [
-    '<rootDir>/ts',
-  ],
-  testMatch: ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
-  transform: {
-    '^.+\\.(js|jsx|mjs)$': 'babel-jest',
-  },
-  watchPlugins: [
-    'jest-watch-suspend',
-    'jest-watch-typeahead/filename',
-    'jest-watch-typeahead/testname',
-    [
-      'jest-watch-toggle-config', { 'setting': 'verbose' },
-    ],
-    [
-      'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
-    ],
-  ],
+  preset: '@repobuddy/jest/presets/ts-esm-watch'
 }
